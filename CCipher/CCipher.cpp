b69baa07de2c99a8a2c4d415fe486a9408876972
anson <anson627@gmail.com>
2013-03-22 21:55:36 -0700

    finalize four

--- CCipher/CCipher.cpp
+++ CCipher/CCipher.cpp
@@ -23,8 +23,8 @@ using namespace std;
 
 class CCipher {
 public:
-	string decode(string cipherText, int shift) {
-	    for(size_t i = 0; i < cipherText.size(); i++) {
+    string decode(string cipherText, int shift) {
+        for(size_t i = 0; i < cipherText.size(); i++) {
             int j = cipherText[i];
             j -= shift;
             if (j < 'A') j += 26;
@@ -42,108 +42,108 @@ public:
 #include <vector>
 using namespace std;
 bool KawigiEdit_RunTest(int testNum, string p0, int p1, bool hasAnswer, string p2) {
-	cout << "Test " << testNum << ": [" << "\"" << p0 << "\"" << "," << p1;
-	cout << "]" << endl;
-	CCipher *obj;
-	string answer;
-	obj = new CCipher();
-	clock_t startTime = clock();
-	answer = obj->decode(p0, p1);
-	clock_t endTime = clock();
-	delete obj;
-	bool res;
-	res = true;
-	cout << "Time: " << double(endTime - startTime) / CLOCKS_PER_SEC << " seconds" << endl;
-	if (hasAnswer) {
-		cout << "Desired answer:" << endl;
-		cout << "\t" << "\"" << p2 << "\"" << endl;
-	}
-	cout << "Your answer:" << endl;
-	cout << "\t" << "\"" << answer << "\"" << endl;
-	if (hasAnswer) {
-		res = answer == p2;
-	}
-	if (!res) {
-		cout << "DOESN'T MATCH!!!!" << endl;
-	} else if (double(endTime - startTime) / CLOCKS_PER_SEC >= 2) {
-		cout << "FAIL the timeout" << endl;
-		res = false;
-	} else if (hasAnswer) {
-		cout << "Match :-)" << endl;
-	} else {
-		cout << "OK, but is it right?" << endl;
-	}
-	cout << "" << endl;
-	return res;
+    cout << "Test " << testNum << ": [" << "\"" << p0 << "\"" << "," << p1;
+    cout << "]" << endl;
+    CCipher *obj;
+    string answer;
+    obj = new CCipher();
+    clock_t startTime = clock();
+    answer = obj->decode(p0, p1);
+    clock_t endTime = clock();
+    delete obj;
+    bool res;
+    res = true;
+    cout << "Time: " << double(endTime - startTime) / CLOCKS_PER_SEC << " seconds" << endl;
+    if (hasAnswer) {
+        cout << "Desired answer:" << endl;
+        cout << "\t" << "\"" << p2 << "\"" << endl;
+    }
+    cout << "Your answer:" << endl;
+    cout << "\t" << "\"" << answer << "\"" << endl;
+    if (hasAnswer) {
+        res = answer == p2;
+    }
+    if (!res) {
+        cout << "DOESN'T MATCH!!!!" << endl;
+    } else if (double(endTime - startTime) / CLOCKS_PER_SEC >= 2) {
+        cout << "FAIL the timeout" << endl;
+        res = false;
+    } else if (hasAnswer) {
+        cout << "Match :-)" << endl;
+    } else {
+        cout << "OK, but is it right?" << endl;
+    }
+    cout << "" << endl;
+    return res;
 }
 int main() {
-	bool all_right;
-	all_right = true;
-	
-	string p0;
-	int p1;
-	string p2;
-	
-	{
-	// ----- test 0 -----
-	p0 = "VQREQFGT";
-	p1 = 2;
-	p2 = "TOPCODER";
-	all_right = KawigiEdit_RunTest(0, p0, p1, true, p2) && all_right;
-	// ------------------
-	}
-	
-	{
-	// ----- test 1 -----
-	p0 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	p1 = 10;
-	p2 = "QRSTUVWXYZABCDEFGHIJKLMNOP";
-	all_right = KawigiEdit_RunTest(1, p0, p1, true, p2) && all_right;
-	// ------------------
-	}
-	
-	{
-	// ----- test 2 -----
-	p0 = "TOPCODER";
-	p1 = 0;
-	p2 = "TOPCODER";
-	all_right = KawigiEdit_RunTest(2, p0, p1, true, p2) && all_right;
-	// ------------------
-	}
-	
-	{
-	// ----- test 3 -----
-	p0 = "ZWBGLZ";
-	p1 = 25;
-	p2 = "AXCHMA";
-	all_right = KawigiEdit_RunTest(3, p0, p1, true, p2) && all_right;
-	// ------------------
-	}
-	
-	{
-	// ----- test 4 -----
-	p0 = "DBNPCBQ";
-	p1 = 1;
-	p2 = "CAMOBAP";
-	all_right = KawigiEdit_RunTest(4, p0, p1, true, p2) && all_right;
-	// ------------------
-	}
-	
-	{
-	// ----- test 5 -----
-	p0 = "LIPPSASVPH";
-	p1 = 4;
-	p2 = "HELLOWORLD";
-	all_right = KawigiEdit_RunTest(5, p0, p1, true, p2) && all_right;
-	// ------------------
-	}
-	
-	if (all_right) {
-		cout << "You're a stud (at least on the example cases)!" << endl;
-	} else {
-		cout << "Some of the test cases had errors." << endl;
-	}
-	return 0;
+    bool all_right;
+    all_right = true;
+
+    string p0;
+    int p1;
+    string p2;
+
+    {
+        // ----- test 0 -----
+        p0 = "VQREQFGT";
+        p1 = 2;
+        p2 = "TOPCODER";
+        all_right = KawigiEdit_RunTest(0, p0, p1, true, p2) && all_right;
+        // ------------------
+    }
+
+    {
+        // ----- test 1 -----
+        p0 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        p1 = 10;
+        p2 = "QRSTUVWXYZABCDEFGHIJKLMNOP";
+        all_right = KawigiEdit_RunTest(1, p0, p1, true, p2) && all_right;
+        // ------------------
+    }
+
+    {
+        // ----- test 2 -----
+        p0 = "TOPCODER";
+        p1 = 0;
+        p2 = "TOPCODER";
+        all_right = KawigiEdit_RunTest(2, p0, p1, true, p2) && all_right;
+        // ------------------
+    }
+
+    {
+        // ----- test 3 -----
+        p0 = "ZWBGLZ";
+        p1 = 25;
+        p2 = "AXCHMA";
+        all_right = KawigiEdit_RunTest(3, p0, p1, true, p2) && all_right;
+        // ------------------
+    }
+
+    {
+        // ----- test 4 -----
+        p0 = "DBNPCBQ";
+        p1 = 1;
+        p2 = "CAMOBAP";
+        all_right = KawigiEdit_RunTest(4, p0, p1, true, p2) && all_right;
+        // ------------------
+    }
+
+    {
+        // ----- test 5 -----
+        p0 = "LIPPSASVPH";
+        p1 = 4;
+        p2 = "HELLOWORLD";
+        all_right = KawigiEdit_RunTest(5, p0, p1, true, p2) && all_right;
+        // ------------------
+    }
+
+    if (all_right) {
+        cout << "You're a stud (at least on the example cases)!" << endl;
+    } else {
+        cout << "Some of the test cases had errors." << endl;
+    }
+    return 0;
 }
 // END KAWIGIEDIT TESTING
 //Powered by KawigiEdit 2.1.8 (beta) modified by pivanof!
--- RectangularGrid/RectangularGrid.cpp
+++ RectangularGrid/RectangularGrid.cpp
@@ -23,8 +23,8 @@ using namespace std;
 
 class RectangularGrid {
 public:
-	long long countRectangles(int width, int height) {
-	    long long res = 0;
+    long long countRectangles(int width, int height) {
+        long long res = 0;
         for (int i = 1; i <= width; i++) {
             for (int j = 1; j <= height; j++) {
                 if (i == j) continue;
@@ -32,7 +32,7 @@ public:
             }
         }
         return res;
-	}
+    }
 };
 
 
@@ -43,99 +43,99 @@ public:
 #include <vector>
 using namespace std;
 bool KawigiEdit_RunTest(int testNum, int p0, int p1, bool hasAnswer, long long p2) {
-	cout << "Test " << testNum << ": [" << p0 << "," << p1;
-	cout << "]" << endl;
-	RectangularGrid *obj;
-	long long answer;
-	obj = new RectangularGrid();
-	clock_t startTime = clock();
-	answer = obj->countRectangles(p0, p1);
-	clock_t endTime = clock();
-	delete obj;
-	bool res;
-	res = true;
-	cout << "Time: " << double(endTime - startTime) / CLOCKS_PER_SEC << " seconds" << endl;
-	if (hasAnswer) {
-		cout << "Desired answer:" << endl;
-		cout << "\t" << p2 << endl;
-	}
-	cout << "Your answer:" << endl;
-	cout << "\t" << answer << endl;
-	if (hasAnswer) {
-		res = answer == p2;
-	}
-	if (!res) {
-		cout << "DOESN'T MATCH!!!!" << endl;
-	} else if (double(endTime - startTime) / CLOCKS_PER_SEC >= 2) {
-		cout << "FAIL the timeout" << endl;
-		res = false;
-	} else if (hasAnswer) {
-		cout << "Match :-)" << endl;
-	} else {
-		cout << "OK, but is it right?" << endl;
-	}
-	cout << "" << endl;
-	return res;
+    cout << "Test " << testNum << ": [" << p0 << "," << p1;
+    cout << "]" << endl;
+    RectangularGrid *obj;
+    long long answer;
+    obj = new RectangularGrid();
+    clock_t startTime = clock();
+    answer = obj->countRectangles(p0, p1);
+    clock_t endTime = clock();
+    delete obj;
+    bool res;
+    res = true;
+    cout << "Time: " << double(endTime - startTime) / CLOCKS_PER_SEC << " seconds" << endl;
+    if (hasAnswer) {
+        cout << "Desired answer:" << endl;
+        cout << "\t" << p2 << endl;
+    }
+    cout << "Your answer:" << endl;
+    cout << "\t" << answer << endl;
+    if (hasAnswer) {
+        res = answer == p2;
+    }
+    if (!res) {
+        cout << "DOESN'T MATCH!!!!" << endl;
+    } else if (double(endTime - startTime) / CLOCKS_PER_SEC >= 2) {
+        cout << "FAIL the timeout" << endl;
+        res = false;
+    } else if (hasAnswer) {
+        cout << "Match :-)" << endl;
+    } else {
+        cout << "OK, but is it right?" << endl;
+    }
+    cout << "" << endl;
+    return res;
 }
 int main() {
-	bool all_right;
-	all_right = true;
-	
-	int p0;
-	int p1;
-	long long p2;
-	
-	{
-	// ----- test 0 -----
-	p0 = 3;
-	p1 = 3;
-	p2 = 22ll;
-	all_right = KawigiEdit_RunTest(0, p0, p1, true, p2) && all_right;
-	// ------------------
-	}
-	
-	{
-	// ----- test 1 -----
-	p0 = 5;
-	p1 = 2;
-	p2 = 31ll;
-	all_right = KawigiEdit_RunTest(1, p0, p1, true, p2) && all_right;
-	// ------------------
-	}
-	
-	{
-	// ----- test 2 -----
-	p0 = 10;
-	p1 = 10;
-	p2 = 2640ll;
-	all_right = KawigiEdit_RunTest(2, p0, p1, true, p2) && all_right;
-	// ------------------
-	}
-	
-	{
-	// ----- test 3 -----
-	p0 = 1;
-	p1 = 1;
-	p2 = 0ll;
-	all_right = KawigiEdit_RunTest(3, p0, p1, true, p2) && all_right;
-	// ------------------
-	}
-	
-	{
-	// ----- test 4 -----
-	p0 = 592;
-	p1 = 964;
-	p2 = 81508708664ll;
-	all_right = KawigiEdit_RunTest(4, p0, p1, true, p2) && all_right;
-	// ------------------
-	}
-	
-	if (all_right) {
-		cout << "You're a stud (at least on the example cases)!" << endl;
-	} else {
-		cout << "Some of the test cases had errors." << endl;
-	}
-	return 0;
+    bool all_right;
+    all_right = true;
+
+    int p0;
+    int p1;
+    long long p2;
+
+    {
+        // ----- test 0 -----
+        p0 = 3;
+        p1 = 3;
+        p2 = 22ll;
+        all_right = KawigiEdit_RunTest(0, p0, p1, true, p2) && all_right;
+        // ------------------
+    }
+
+    {
+        // ----- test 1 -----
+        p0 = 5;
+        p1 = 2;
+        p2 = 31ll;
+        all_right = KawigiEdit_RunTest(1, p0, p1, true, p2) && all_right;
+        // ------------------
+    }
+
+    {
+        // ----- test 2 -----
+        p0 = 10;
+        p1 = 10;
+        p2 = 2640ll;
+        all_right = KawigiEdit_RunTest(2, p0, p1, true, p2) && all_right;
+        // ------------------
+    }
+
+    {
+        // ----- test 3 -----
+        p0 = 1;
+        p1 = 1;
+        p2 = 0ll;
+        all_right = KawigiEdit_RunTest(3, p0, p1, true, p2) && all_right;
+        // ------------------
+    }
+
+    {
+        // ----- test 4 -----
+        p0 = 592;
+        p1 = 964;
+        p2 = 81508708664ll;
+        all_right = KawigiEdit_RunTest(4, p0, p1, true, p2) && all_right;
+        // ------------------
+    }
+
+    if (all_right) {
+        cout << "You're a stud (at least on the example cases)!" << endl;
+    } else {
+        cout << "Some of the test cases had errors." << endl;
+    }
+    return 0;
 }
 // END KAWIGIEDIT TESTING
 //Powered by KawigiEdit 2.1.8 (beta) modified by pivanof!
